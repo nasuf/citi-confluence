db5da9d80d2e6cb572c0c62f2b8b6e06d098f8a1
mingyue <329408093@qq.com>
2020-09-17 12:47:09 +0800

    flow list

--- src/app/flow-list.ts
+++ src/app/flow-list.ts
@@ -1,17 +1,21 @@
 export const flowList = [
     {
-        name: 'first flow information',
+        title: 'Intergrating WordPress with Your Website',
         author: "lily",
-        createTime: '2020-09-17'
+        createTime: '2020-09-17',
+        technical: "Server && Database",
+        comments: 3,
+        likes: 13,
+        content: "Intergrating WordPress with Your Website Intergrating WordPress with Your Website, Intergrating WordPress with Your Website,Intergrating WordPress with Your Website, Intergrating WordPress with Your Website,Intergrating WordPress with Your Website"
     },
-    {
-        name: 'second flow information',
-        author: "Alan",
-        createTime: '2020-09-20'
-    },
-    {
-        name: 'third flow information',
-        author: "Dyddy",
-        createTime: '2020-09-13'
-    }
+    // {
+    //     name: 'second flow information',
+    //     author: "Alan",
+    //     createTime: '2020-09-20'
+    // },
+    // {
+    //     name: 'third flow information',
+    //     author: "Dyddy",
+    //     createTime: '2020-09-13'
+    // }
   ];
